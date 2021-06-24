60ad35baf769bb59d17ecf0a0b70b993a02828b9
Jonathan Fritz <jfritz@1-B-5.42heilbronn.de>
2021-06-24 16:28:43 +0200

    stuff

---
+++ ft_get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_get_next_line.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfritz <jfritz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/24 09:49:36 by jfritz            #+#    #+#             */
+/*   Updated: 2021/06/24 10:52:23 by jfritz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+int get_next_line(int fd, char **line);
+
+char	*ft_strchr(const char *s, int c);
+
+#endif
